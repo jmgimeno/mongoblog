98a284c327f41003ad77f7bc115c85e66a8cba16
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2018-04-02 15:29:39 +0200

    Update versions of some dependencies.

--- src/main/java/course/BlogController.java
+++ src/main/java/course/BlogController.java
@@ -20,8 +20,7 @@ package course;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
-import freemarker.template.SimpleHash;
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.bson.Document;
 import spark.ModelAndView;
 import spark.Request;
@@ -34,7 +33,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.post;
 
 /**
  * This class encapsulates the controllers for the blog web application.  It delegates all interaction with MongoDB
@@ -48,35 +48,34 @@ public class BlogController {
     private final SessionDAO sessionDAO;
     private final FreeMarkerEngine freeMarkerEngine;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         if (args.length == 0) {
             new BlogController("mongodb://mongo:27017");
-        }
-        else {
+        } else {
             new BlogController(args[0]);
         }
     }
 
-    public BlogController(String mongoURIString) throws IOException {
+    public BlogController(String mongoURIString) {
         MongoClient mongoClient = new MongoClient(new MongoClientURI(mongoURIString));
         MongoDatabase blogDatabase = mongoClient.getDatabase("blog");
 
-        blogPostDAO      = new BlogPostDAO(blogDatabase);
-        userDAO          = new UserDAO(blogDatabase);
-        sessionDAO       = new SessionDAO(blogDatabase);
+        blogPostDAO = new BlogPostDAO(blogDatabase);
+        userDAO = new UserDAO(blogDatabase);
+        sessionDAO = new SessionDAO(blogDatabase);
         freeMarkerEngine = new FreeMarkerEngine();
 
         initializeRoutes();
     }
 
-    private void initializeRoutes() throws IOException {
+    private void initializeRoutes() {
 
         // this is the blog home page
         get("/", (request, response) -> {
             String username = sessionDAO.findUserNameBySessionId(getSessionCookie(request));
             List<Document> posts = blogPostDAO.findByDateDescending(10);
-            SimpleHash root = new SimpleHash();
+            Map<String, Object> root = new HashMap<>();
             root.put("myposts", posts);
             if (username != null) {
                 root.put("username", username);
@@ -89,23 +88,18 @@ public class BlogController {
             String permalink = request.params(":permalink");
             System.out.println("/post: get " + permalink);
             Document post = blogPostDAO.findByPermalink(permalink);
-
             if (post == null) {
                 response.redirect("/post_not_found");
                 return "";
             } else {
-
                 // empty comment to hold new comment in form at bottom of blog entry detail page
-                SimpleHash newComment = new SimpleHash();
+                Map<String, Object> newComment = new HashMap<>();
                 newComment.put("name", "");
                 newComment.put("email", "");
                 newComment.put("body", "");
-
-                SimpleHash root = new SimpleHash();
-
+                Map<String, Object> root = new HashMap<>();
                 root.put("post", post);
                 root.put("comment", newComment);
-
                 return freeMarkerEngine.render(new ModelAndView(root, "entry_template.ftl"));
             }
         });
@@ -116,11 +110,9 @@ public class BlogController {
             String username = request.queryParams("username");
             String password = request.queryParams("password");
             String verify = request.queryParams("verify");
-
             Map<String, String> root = new HashMap<>();
             root.put("username", StringEscapeUtils.escapeHtml4(username));
             root.put("email", StringEscapeUtils.escapeHtml4(email));
-
             if (validateSignup(username, password, verify, email, root)) {
                 // good user
                 System.out.println("Signup: Creating user with: " + username + " " + password);
@@ -128,18 +120,15 @@ public class BlogController {
                     // duplicate user
                     root.put("username_error", "Username already in use, Please choose another");
                     return freeMarkerEngine.render(new ModelAndView(root, "signup.ftl"));
-                }
-                else {
+                } else {
                     // good user, let's start a session
                     String sessionID = sessionDAO.startSession(username);
                     System.out.println("Session ID is" + sessionID);
-
                     response.raw().addCookie(new Cookie("session", sessionID));
                     response.redirect("/welcome");
                     return "";
                 }
-            }
-            else {
+            } else {
                 // bad signup
                 System.out.println("User Registration did not validate");
                 return freeMarkerEngine.render(new ModelAndView(root, "signup.ftl"));
@@ -148,8 +137,7 @@ public class BlogController {
 
         // present signup form for blog
         get("/signup", (request, response) -> {
-            SimpleHash root = new SimpleHash();
-
+            Map<String, Object> root = new HashMap<>();
             // initialize values for the form.
             root.put("username", "");
             root.put("password", "");
@@ -158,7 +146,6 @@ public class BlogController {
             root.put("username_error", "");
             root.put("email_error", "");
             root.put("verify_error", "");
-
             return new ModelAndView(root, "signup.ftl");
         }, freeMarkerEngine);
 
@@ -166,16 +153,13 @@ public class BlogController {
         get("/newpost", (request, response) -> {
             // get cookie
             String username = sessionDAO.findUserNameBySessionId(getSessionCookie(request));
-
             if (username == null) {
                 // looks like a bad request. user is not logged in
                 response.redirect("/login");
                 return "";
-            }
-            else {
-                SimpleHash root = new SimpleHash();
+            } else {
+                Map<String, Object> root = new HashMap<>();
                 root.put("username", username);
-
                 return freeMarkerEngine.render(new ModelAndView(root, "newpost_template.ftl"));
             }
         });
@@ -185,14 +169,11 @@ public class BlogController {
             String title = StringEscapeUtils.escapeHtml4(request.queryParams("subject"));
             String post = StringEscapeUtils.escapeHtml4(request.queryParams("body"));
             String tags = StringEscapeUtils.escapeHtml4(request.queryParams("tags"));
-
             String username = sessionDAO.findUserNameBySessionId(getSessionCookie(request));
-
             if (username == null) {
                 response.redirect("/login");    // only logged in users can post to blog
                 return "";
-            }
-            else if (title.equals("") || post.equals("")) {
+            } else if (title.equals("") || post.equals("")) {
                 // redisplay page with errors
                 Map<String, String> root = new HashMap<>();
                 root.put("errors", "post must contain a title and blog entry.");
@@ -201,16 +182,12 @@ public class BlogController {
                 root.put("tags", tags);
                 root.put("body", post);
                 return freeMarkerEngine.render(new ModelAndView(root, "newpost_template.ftl"));
-            }
-            else {
+            } else {
                 // extract tags
                 List<String> tagsArray = extractTags(tags);
-
                 // substitute some <p> for the paragraph breaks
                 post = post.replaceAll("\\r?\\n", "<p>");
-
                 String permalink = blogPostDAO.addPost(title, post, tagsArray, username);
-
                 // now redirect to the blog permalink
                 response.redirect("/post/" + permalink);
                 return "";
@@ -226,10 +203,8 @@ public class BlogController {
                 System.out.println("welcome() can't identify the user, redirecting to signup");
                 response.redirect("/signup");
                 return "";
-            }
-            else {
-                SimpleHash root = new SimpleHash();
-
+            } else {
+                Map<String, Object> root = new HashMap<>();
                 root.put("username", username);
 
                 return freeMarkerEngine.render(new ModelAndView(root, "welcome.ftl"));
@@ -251,19 +226,16 @@ public class BlogController {
             // check that comment is good
             else if (name.equals("") || body.equals("")) {
                 // bounce this back to the user for correction
-                SimpleHash root = new SimpleHash();
-                SimpleHash comment = new SimpleHash();
-
+                Map<String, Object> root = new HashMap<>();
+                Map<String, Object> comment = new HashMap<>();
                 comment.put("name", name);
                 comment.put("email", email);
                 comment.put("body", body);
                 root.put("comment", comment);
                 root.put("post", post);
                 root.put("errors", "Post must contain your name and an actual comment");
-
                 return freeMarkerEngine.render(new ModelAndView(root, "entry_template.ftl"));
-            }
-            else {
+            } else {
                 blogPostDAO.addPostComment(name, email, body, permalink);
                 response.redirect("/post/" + permalink);
                 return "";
@@ -272,8 +244,7 @@ public class BlogController {
 
         // present the login page
         get("/login", (request, response) -> {
-            SimpleHash root = new SimpleHash();
-
+            Map<String, Object> root = new HashMap<>();
             root.put("username", "");
             root.put("login_error", "");
 
@@ -285,30 +256,22 @@ public class BlogController {
         post("/login", (request, response) -> {
             String username = request.queryParams("username");
             String password = request.queryParams("password");
-
             System.out.println("Login: User submitted: " + username + "  " + password);
-
             Document user = userDAO.validateLogin(username, password);
-
             if (user != null) {
-
                 // valid user, let's log them in
                 String sessionID = sessionDAO.startSession(user.get("_id").toString());
-
                 if (sessionID == null) {
                     response.redirect("/internal_error");
-                }
-                else {
+                } else {
                     // set the cookie for the user's browser
                     response.raw().addCookie(new Cookie("session", sessionID));
 
                     response.redirect("/welcome");
                 }
                 return "";
-            }
-            else {
-                SimpleHash root = new SimpleHash();
-
+            } else {
+                Map<String, Object> root = new HashMap<>();
                 root.put("username", StringEscapeUtils.escapeHtml4(username));
                 root.put("password", "");
                 root.put("login_error", "Invalid Login");
@@ -319,37 +282,31 @@ public class BlogController {
         // show the posts filed under a certain tag
         get("/tag/:thetag", (request, response) -> {
             String username = sessionDAO.findUserNameBySessionId(getSessionCookie(request));
-            SimpleHash root = new SimpleHash();
-
+            Map<String, Object> root = new HashMap<>();
             String tag = StringEscapeUtils.escapeHtml4(request.params(":thetag"));
             List<Document> posts = blogPostDAO.findByTagDateDescending(tag);
-
             root.put("myposts", posts);
             if (username != null) {
                 root.put("username", username);
             }
-
             return new ModelAndView(root, "blog_template.ftl");
         }, freeMarkerEngine);
 
         // tells the user that the URL is dead
         get("/post_not_found", (request, response) -> {
-            SimpleHash root = new SimpleHash();
+            Map<String, Object> root = new HashMap<>();
             return new ModelAndView(root, "post_not_found.ftl");
         }, freeMarkerEngine);
 
         // allows the user to logout of the blog
         get("/logout", (request, response) -> {
             String sessionID = getSessionCookie(request);
-
             if (sessionID == null) {
                 // no session to end
                 response.redirect("/login");
-            }
-            else {
+            } else {
                 // deletes from session table
                 sessionDAO.endSession(sessionID);
-
                 // this should delete the cookie
                 Cookie c = getSessionCookieActual(request);
                 c.setMaxAge(0);
@@ -361,7 +318,7 @@ public class BlogController {
 
         // used to process internal errors
         get("/internal_error", (request, response) -> {
-            SimpleHash root = new SimpleHash();
+            Map<String, Object> root = new HashMap<>();
             root.put("error", "System has encountered an error.");
             return new ModelAndView(root, "error_template.ftl");
         }, freeMarkerEngine);
@@ -395,14 +352,11 @@ public class BlogController {
 
     // tags the tags string and put it into an array
     private ArrayList<String> extractTags(String tags) {
-
         // probably more efficient ways to do this.
         //
         // whitespace = re.compile('\s')
-
         tags = tags.replaceAll("\\s", "");
         String tagArray[] = tags.split(",");
-
         // let's clean it up, removing the empty string and removing dups
         ArrayList<String> cleaned = new ArrayList<>();
         for (String tag : tagArray) {
@@ -410,13 +364,12 @@ public class BlogController {
                 cleaned.add(tag);
             }
         }
-
         return cleaned;
     }
 
     // validates that the registration form has been filled out right and username conforms
-    public boolean validateSignup(String username, String password, String verify, String email,
-                                  Map<String, String> errors) {
+    private boolean validateSignup(String username, String password, String verify, String email,
+                                   Map<String, String> errors) {
         String USER_RE = "^[a-zA-Z0-9_-]{3,20}$";
         String PASS_RE = "^.{3,20}$";
         String EMAIL_RE = "^[\\S]+@[\\S]+\\.[\\S]+$";
